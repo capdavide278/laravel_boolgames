93e3cb6bc111727aa14ec49ed8f66bea35106d5f
Davide <franc.davi00@gmail.com>
2022-08-25 17:27:20 +0200

    push verifica

--- app/Videogames.php
+++ app/Videogames.php
@@ -7,10 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Videogames extends Model
 {
     protected $fillable = [
-<<<<<<< HEAD
         'nome', 'descrizione', 'img'
-=======
-        'nome', 'descrizione','img'
->>>>>>> 499a09456de6d1eeb15540433cfab1c715077758
     ];
 }
